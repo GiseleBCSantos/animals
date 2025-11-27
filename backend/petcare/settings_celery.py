a89189638d1fb1bcdc6d98794cb041d665f6b4b0
GiseleBCSantos <giselebianca57@gmail.com>
2025-11-27 07:55:56 -0300

    :recycle: refactor: update Celery task schedule to run daily at 07:00

--- backend/petcare/settings_celery.py
+++ backend/petcare/settings_celery.py
@@ -5,6 +5,6 @@ from datetime import timedelta
 CELERY_BEAT_SCHEDULE = {
     'gerar-pensamentos-todos-os-dias-07': {
         'task': 'animals.tasks.gerar_pensamentos_diarios',
-        "schedule": crontab(minute="*"),
+        "schedule": crontab(hour=7, minute=0),
     }
 }
